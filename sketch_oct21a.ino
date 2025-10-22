43abbefd2801d0907583cd9fc6cb9dd90bc96abb
OkuJoe <joeokurut776@gmail.com>
2025-10-22 06:08:56 -0700

    Add files via upload

---
+++ sketch_oct21a.ino
@@ -0,0 +1,16 @@
+
+
+string hello=" hello Bro";
+void setup() {
+  // put your setup code here, to run once:
+// it is used to turn on monitor
+Serial.begin(9600); // set a board rate such that it can apear on serial monior
+Serial.print(hello);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
